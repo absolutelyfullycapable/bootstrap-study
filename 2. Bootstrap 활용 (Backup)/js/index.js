16b9f404c4f053650fea84ff3db29acccae08dfe
absolutelyfullycapable <lovinsurfaces@gmail.com>
2020-12-23 21:12:35 +0900

    update backup files

--- 2. Bootstrap 활용 (Backup)/js/index.js
+++ 2. Bootstrap 활용 (Backup)/js/index.js
@@ -1,19 +1,21 @@
   $(document).ready(function(){
 
     $('a[href="#"]').click(function(e){
-                        e.preventDefault();
-                       });
+        e.preventDefault();
+    });
 
-        //nav 버튼 변경, 배경색
+    //nav 버튼 변경, 배경색
     $('.btn-menu').click(function(){
         $('.btn-menu').css('display','none');
         $('.btn-menu-close').css('display','block');
         $('.nav_bg').css('display','block');
+        $('html').css('overflow', 'hidden');
     });
     $('.btn-menu-close').click(function(){
         $('.btn-menu-close').css('display','none');
         $('.btn-menu').css('display','block');
         $('.nav_bg').css('display','none');
+        $('html').css('overflow', 'initial');
     });
 
 
@@ -72,7 +74,6 @@
 
 
     //location map
-
     var btn_daejeon = $('#main #location .btn_group .btn_daejeon');
     var btn_jeju = $('#main #location .btn_group .btn_jeju');
     var map_daejeon = $('#main #location .map .daejeon');
--- 2. Bootstrap 활용 (Backup)/js/support.js
+++ 2. Bootstrap 활용 (Backup)/js/support.js
@@ -1,14 +1,46 @@
- $(document).ready(function(){
+$(document).ready(function(){
+    $('a[href="#"]').click(function(e){
+        e.preventDefault();
+    });
+
+    //nav 버튼 변경, 배경색
+    $('.btn-menu').click(function(){
+        $('.btn-menu').css('display','none');
+        $('.btn-menu-close').css('display','block');
+        $('.nav_bg').css('display','block');
+        $('html').css('overflow', 'hidden');
+    });
+    $('.btn-menu-close').click(function(){
+        $('.btn-menu-close').css('display','none');
+        $('.btn-menu').css('display','block');
+        $('.nav_bg').css('display','none');
+        $('html').css('overflow', 'initial');
+    });
+
     //accodian menu
-    $('.faq-board').find('a').on('click',function(){
-        var inx= $(this).parents('li').index();
-        $(this).parents('li').addClass('on').siblings().removeClass('on');
-        if($(this).parents('li').is(':visible')){
-            $(this).next().slideUp();
-            $('.support-faq-img').find('li').removeClass('on');
-        }
-        $('.faq-list p').slideUp();
-        $(this).next().slideDown();
+    $('.faq-box').hide();
+
+    $('.faq-list a').on('click', function() {
+        var inx = $(this).parents('li').index();
+        
+        $(this).parents('li').toggleClass('on');
+        $('.faq-box').stop().slideUp();
+        $(this).next('.faq-box').stop().slideDown();
         $('.support-faq-img').find('li').eq(inx).addClass('on').siblings().removeClass('on');
+
+        if($(this).parents('li').hasClass('on')) {
+            $(this).parents('li').siblings().removeClass('on');
+            $(this).on('click', function() {
+                $(this).next('.faq-box').stop().slideUp();
+                $(this).parents('li').removeClass('on');
+                $('.support-faq-img').find('li').eq(inx).removeClass('on');
+            });
+        } else {
+            $(this).on('click', function() {
+                $(this).next('.faq-box').stop().slideDown();
+                $(this).parents('li').addClass('on');
+                $('.support-faq-img').find('li').eq(inx).addClass('on');
+            });
+        }
     });
 });
